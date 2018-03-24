ba4bada869dd26c2d9ee1e955244e5b1ad34aef5
Dan Schrimpsher <d.schrimpsher@cablelabs.com>
2018-03-24 14:41:50 -0500

    AH!!!!!

--- ui/src/views/ImportExport/PullNetworks.js
+++ ui/src/views/ImportExport/PullNetworks.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
-import { RingLoader } from 'react-spinners';
+import {RingLoader} from 'react-spinners';
 
 
 import networkTypeStore from "../../stores/NetworkTypeStore";
@@ -14,40 +14,40 @@ class PullNetworks extends Component {
             networkTypeId: this.props.match.params.networkTypeId
         };
         networkTypeStore.pullNetworkType(this.props.match.params.networkTypeId)
-            .then( function() {
+            .then(function () {
                 this.setState({loading: false, error: false});
             })
-            .catch(function(err) {
+            .catch(function (err) {
                 this.setState({loading: false, error: true});
             })
 
     }
 
 
-
     render() {
         return (
-            <div>{this.state.error) && <div>Error Importing Data from Network Server</div>}
-            <div>
-                <ol className="breadcrumb">
-                    <li><Link to={`/`}>Home</Link></li>
-                    <li><Link to={`/admin/networks`}>Networks</Link></li>
-                </ol>
-              <div className={`panel-body clearfix `}>
-                <table className="table table-hover">
-                    <tbody>
-                <tr>
-                  <td><RingLoader
-                      color={'#123abc'}
-                      loading={this.state.loading}
-                  /></td>
-                  <td>
-                    <p>Importing data from network server ${this.state.networkTypeId}, please wait.</p>
-                  </td>
-                </tr>
-                    </tbody>
-              </table>
-              </div>
+            <div>{this.state.error && <div>Error Importing Data from Network Server</div>}
+                <div>
+                    <ol className="breadcrumb">
+                        <li><Link to={`/`}>Home</Link></li>
+                        <li><Link to={`/admin/networks`}>Networks</Link></li>
+                    </ol>
+                    <div className={`panel-body clearfix `}>
+                        <table className="table table-hover">
+                            <tbody>
+                            <tr>
+                                <td><RingLoader
+                                    color={'#123abc'}
+                                    loading={this.state.loading}
+                                /></td>
+                                <td>
+                                    <p>Importing data from network server ${this.state.networkTypeId}, please wait.</p>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         )
     }
