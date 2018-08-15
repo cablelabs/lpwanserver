bb1fac5e0f36f00be89c6f7706de06c324a2b7f5
Steven Saunders <steve@sentosatech.com>
2018-08-15 08:43:16 -0600

    cleanup

--- ui/src/components/Navbar.js
+++ ui/src/components/Navbar.js
@@ -75,20 +75,7 @@ class Navbar extends Component {
 
                 <li className={this.state.isGlobalAdmin === true ? "" : "hidden"}><a href="/admin/companies">Companies</a></li>
                 <li className={this.state.isGlobalAdmin === true ? "" : "hidden"}><a href="/admin/networks">Networks</a></li>
-
-                { /*
-                <li className={"dropdown " + (this.state.isGlobalAdmin === true ? "" : "hidden") + (this.state.networkDropdownOpen ? "open" : "")}>
-                    <a onClick={this.networkToggleDropdown} className="dropdown-toggle">Networks <span className="caret" /></a>
-                    <ul className="dropdown-menu" onClick={this.networkToggleDropdown}>
-                  <li><a href={`/admin/networkTypes`}>Network Types</a></li>
-                  <li><a href={`/admin/networkProviders`}>Network Providers</a></li>
-                  <li><a href={`/admin/networkProtocols`}>Network Protocols</a></li>
-                  <li><a href={`/admin/networks`}>Networks</a></li>
-                </ul>
-               </li> */ }
-
-               <li className={this.state.isGlobalAdmin === true ? "" : "hidden"}><a href="/admin/reportingProtocols">Application Reporting Protocols</a></li>
-
+                <li className={this.state.isGlobalAdmin === true ? "" : "hidden"}><a href="/admin/reportingProtocols">Application Reporting Protocols</a></li>
                 <li className={"dropdown " + (typeof(this.state.user.username) === "undefined" ? "hidden" : "") + (this.state.userDropdownOpen ? "open" : "")}>
                   <a onClick={this.userToggleDropdown} className="dropdown-toggle">{this.state.user.username} <span className="caret" /></a>
                   <ul className="dropdown-menu" onClick={this.userToggleDropdown}>
