47335486c4e11ec749d94285333ebac9ab7f93d1
Nick Baroni <nick@rhythnic.com>
2018-10-12 16:32:43 -0400

    Move demo data removal and copy to package script.

--- bin/package.js
+++ bin/package.js
@@ -2,15 +2,16 @@ const path = require('path')
 const component =require('../component.json')
 const { execSync } = require('child_process')
 
-const atPath = (...args) => path.join(__dirname, ...args)
+const ROOT = path.join(__dirname, '..')
+const opts = { cwd: ROOT, stdio: 'inherit' }
 
 const image = `${component.registry}/${component.name}:${component.version}-${component.build}-rc`
 const latestImage = `${component.registry}/${component.name}:latest`
 
-const cwd = atPath('..')
-
 async function package () {
-  execSync(`docker build -f docker/Dockerfile -t ${image} -t ${latestImage} .`, { cwd, stdio: 'inherit' })
+  execSync('rm -rf ./data/demo_data', opts)
+  execSync('cp -r ./data/demo_baseline ./data/demo_data', opts)
+  execSync(`docker build -f docker/Dockerfile -t ${image} -t ${latestImage} .`, opts)
   console.info('The container was successfully built.')
 }
 
