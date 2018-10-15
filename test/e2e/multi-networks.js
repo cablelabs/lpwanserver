f35c0ce3dee70224253197e763dd0a3f944abe80
Dan Schrimpsher <dan.schrimpsher@gmail.com>
2018-10-15 15:18:00 -0500

    Updated and retested E2E with LoRaServerV1

--- test/e2e/multi-networks.js
+++ test/e2e/multi-networks.js
@@ -10,7 +10,7 @@ var request = require('request')
 chai.use(chaiHttp)
 var server = chai.request(app).keepOpen()
 
-describe.only('E2E Test for Multiple Networks', () => {
+describe('E2E Test for Multiple Networks', () => {
   var adminToken
   var userId
   var userToken
