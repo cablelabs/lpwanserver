b2f2276c0b40f5be543e1624926bdb45aacd899c
Dan Schrimpsher <dan.schrimpsher@gmail.com>
2018-12-12 11:32:38 -0600

    Fixed and tested bugs in update device in Lora V2.

--- config/index.js
+++ config/index.js
@@ -11,4 +11,3 @@ nconf
   .file('node_env_file', { file: `config/${nconf.get('NODE_ENV')}.hjson`, format: hjson })
   // Load defaults
   .file('defaults', { file: 'config/defaults.hjson', format: hjson })
-  
