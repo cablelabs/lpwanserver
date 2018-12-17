1c1c2e697dd0bfefba2ab3d6807e83e3b45586de
Dan Schrimpsher <d.schrimpsher@cablelabs.com>
2018-12-17 12:43:28 -0600

    Removed half done stuff that was not part of this issue.

--- config/index.js
+++ config/index.js
@@ -4,7 +4,7 @@ const hjson = require('hjson')
 // Values higher up override values beneath
 nconf
   // command line
-  .argv() 
+  .argv()
   // environment variables
   .env()
   // Selectively override defaults with {NODE_ENV}.hjson file
