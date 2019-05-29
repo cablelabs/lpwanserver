12d406b71f052d052a85d616adca4365c12a04b4
Nick Baroni <nick@rhythnic.com>
2019-05-29 10:53:08 -0400

    Add script for managing db

--- prisma/scrap.js
+++
@@ -1,8 +0,0 @@
-const { prisma } = require('./generated/prisma-client')
-
-async function main () {
-  const cos = await prisma.companies()
-  console.log(JSON.stringify(cos))
-}
-
-main().catch(e => console.error(e))
