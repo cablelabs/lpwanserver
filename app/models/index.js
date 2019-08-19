ad3e7f9ac94d1b2a8053d3cf8d5d8c044e16d505
Nick Baroni <nick@rhythnic.com>
2019-08-19 17:19:01 -0400

    Update model index

--- app/models/index.js
+++ app/models/index.js
@@ -1,77 +1,26 @@
-// Define the parts of the Model API
-
-// Data models - what can be done with each data type.
-var UserModel = require('./IUser')
-var SessionManagerModel = require('./Session')
-var NetworkProtocolModel = require('./INetworkProtocol')
-var NetworkModel = require('./INetwork')
-var ReportingProtocolModel = require('./IReportingProtocol')
-var ApplicationModel = require('./IApplication')
-var ApplicationNetworkTypeLinkModel = require('./IApplicationNetworkTypeLink')
-var DeviceProfileModel = require('./IDeviceProfile')
-var DeviceModel = require('./IDevice')
-var DeviceNetworkTypeLinkModel = require('./IDeviceNetworkTypeLink')
-var NetworkTypeModel = require('./INetworkType')
-var NetworkProviderModel = require('./INetworkProvider')
-var ProtocolDataModel = require('./IProtocolData')
-var EmailModel = require('./IEmail')
-
 // Network Protocol use.
 var NetworkTypeAPI = require('../networkProtocols/networkTypeApi')
 var NetworkProtocolAPI = require('../networkProtocols/networkProtocols')
 
 function ModelAPI () {
-  // Users.  And start the user email verification background task that
-  // expires old email verification records.
-  this.users = new UserModel(this)
-
-  this.emails = new EmailModel(this.users)
-
-  // The session model, which uses users (for login).
-  this.sessions = new SessionManagerModel(this.users)
-
-  // The networkProtocol model.
-  this.networkProtocols = new NetworkProtocolModel()
-
-  // The network model.  Needs the protocols to access the correct api.
-  this.networks = new NetworkModel(this)
-
-  // The network provider model.
-  this.networkProviders = new NetworkProviderModel()
-
-  // The network type model.
-  this.networkTypes = new NetworkTypeModel()
-
-  // The NetworkProvisioningFields model.
-  // this.networkProvisioningFields = new NetworkProvisioningFieldModel();
-
-  // The reportingProtocol model.
-  this.reportingProtocols = new ReportingProtocolModel()
-
-  // The applicationNetworkTypeLink model.
-  this.applicationNetworkTypeLinks = new ApplicationNetworkTypeLinkModel(this)
-
-  // The application model.
-  this.applications = new ApplicationModel(this)
+  this.applications = require('./IApplication').model
+  this.applicationNetworkTypeLinks = require('./IApplicationNetworkTypeLink').model
+  this.devices = require('./IDevice').model
+  this.deviceNetworkTypeLinks = require('./IDeviceNetworkTypeLink').model
+  this.deviceProfile = require('./IDeviceProfile').model
+  this.networks = require('./INetwork').model
+  this.networkProtocols = require('./INetworkProtocol').model
+  this.networkTypes = require('./INetworkType').model
+  this.protocolData = require('./ProtocolData').model
+  this.reportingProtocols = require('./IReportingProtocol').model
+  this.sessions = require('./ISession').model
+  this.users = require('./IUser').model
 
   // The networkProtocol API, giving access to a specific remote network.
   this.networkProtocolAPI = new NetworkProtocolAPI(this)
-
   // The networkType API, giving access to the various remote networks of a
   // given type.
   this.networkTypeAPI = new NetworkTypeAPI(this)
-
-  // The deviceProfile model.
-  this.deviceProfiles = new DeviceProfileModel(this)
-
-  // The device model.  It uses applications for some validation.
-  this.devices = new DeviceModel(this)
-
-  // The applicationNetworkTypeLink model.
-  this.deviceNetworkTypeLinks = new DeviceNetworkTypeLinkModel(this)
-
-  // The helper interface for network protocols to use.
-  this.protocolData = new ProtocolDataModel(this)
 }
 
 ModelAPI.prototype.initialize = async function initializeModelAPI () {
